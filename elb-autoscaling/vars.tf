ea4a20e746f7cbac523f2c58f7996fd19821605e
anandg112 <anandg112@gmail.com>
2018-04-23 02:38:05 -0400

    added ELB-autoscaling

---
+++ elb-autoscaling/vars.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = "map"
+
+  default = {
+    us-east-1 = "ami-5cd4a126"
+    us-west-1 = "ami-e1131781"
+    eu-west-1 = "ami-94b236ed"
+  }
+}
