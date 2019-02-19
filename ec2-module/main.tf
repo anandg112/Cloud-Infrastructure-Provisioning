56195f39465f76969d3b07de529c64228d2adc9a
anandg112 <anandg112@gmail.com>
2019-02-18 22:02:34 -0500

    adding subnet and security group

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -25,10 +25,42 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = "${aws_vpc.my_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    owner = "${var.owner}"
+    team  = "${var.team}"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.my_vpc.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["142.117.37.207/32"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type = "t2.small"
-  key_name      = "${var.key_name}"
+  ami                    = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type          = "t2.small"
+  key_name               = "${var.key_name}"
+  subnet_id              = "${aws_subnet.my_subnet.id}"
+  vpc_security_group_ids = ["${var.aws_security_group.id}"]
 
   tags = {
     owner = "${var.owner}"
