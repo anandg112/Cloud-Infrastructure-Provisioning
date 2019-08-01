21b946af8ef33b71a57dd52d20a52aeca9bf424c
anandg112 <anandg112@gmail.com>
2019-08-01 02:04:49 -0400

    feat: add EKS module

---
+++ EKS/eks-cluster.tf
@@ -0,0 +1,37 @@
+resource "aws_eks_cluster" "anand_deployment" {
+  name     = "${var.cluster_name}"
+  role_arn = "${aws_iam_role.demo-cluster.arn}"
+  vpc_config {
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    security_group_ids = ["${aws_security_group.demo_cluster.id}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    subnet_ids = ["${module.vpc.public_subnets}"]
+  }
+
+#   enabled_cluster_log_types = ["api", "audit"]
+
+  depends_on = [
+    "${aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy}",
+    "${aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy}"
+  ]
+}
+
+# resource "aws_cloudwatch_log_group" "app_logs" {
+#   name              = "/aws/eks/${var.cluster_name}/cluster"
+#   retention_in_days = 7
+# }
+
---
+++ EKS/variables.tf
@@ -0,0 +1,8 @@
+variable "cluster_name" {
+  type = "string"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
